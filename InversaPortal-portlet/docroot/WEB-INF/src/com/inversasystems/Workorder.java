53f1bbef280fee1c7fb6ed837f91d6038b879fae
Glenn Graham <k.glenn.graham@gmail.com>
2013-12-18 13:43:47 -0400

    Initial

---
+++ InversaPortal-portlet/docroot/WEB-INF/src/com/inversasystems/Workorder.java
@@ -0,0 +1,11 @@
+package com.inversasystems;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class Workorder
+ */
+public class Workorder extends MVCPortlet {
+ 
+
+}
